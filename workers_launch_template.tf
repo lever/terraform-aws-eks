edfe5f3f7d2bf705075f5656c8315711e4cae411
Angie Brandt <angiebrandt@gmail.com>
2020-12-10 15:15:02 -0800

    revert

--- workers_launch_template.tf
+++ workers_launch_template.tf
@@ -226,7 +226,7 @@ resource "aws_autoscaling_group" "workers_launch_template" {
 
   lifecycle {
     create_before_destroy = true
-    # ignore_changes        = [desired_capacity]
+    ignore_changes        = [desired_capacity]
   }
 }
 
